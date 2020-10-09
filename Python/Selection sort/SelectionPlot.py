d0b2e2d08ed81fda185b2d9620c48d2c37b54e0e
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-10-09 18:46:38 +0530

    Add files via upload

---
+++ Python/Selection sort/SelectionPlot.py
@@ -0,0 +1,10 @@
+
+    # @Author: Raghav Maheshwari.
+    # @Date: 26th May, 2019
+    # @Topic: Selection Sort
+
+import matplotlib.pyplot as plt
+X = [10000,20000,30000,40000,50000]
+Y = [0.162249,0.662799,1.464096,2.586844,4.027508]
+plt.plot(X,Y)
+plt.show()
