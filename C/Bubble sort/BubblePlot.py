fe5ec5c101c995d6e091c10c748af7270618dcd3
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-10-10 13:21:51 +0530

    Add files via upload

---
+++ C/Bubble sort/BubblePlot.py
@@ -0,0 +1,10 @@
+
+    # @Author: Raghav Maheshwari.
+    # @Date: 26th May, 2019
+    # @Topic: Bubble Sort
+
+import matplotlib.pyplot as plt
+X = [10000,20000,30000,40000,50000]
+Y = [0.355559,1.453964,3.444555,6.122627,10.009010]
+plt.plot(X,Y)
+plt.show()
