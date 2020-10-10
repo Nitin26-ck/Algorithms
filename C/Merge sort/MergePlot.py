986f6a94061da803662245b1c28bcda695dd6c30
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-10-10 13:29:19 +0530

    Update MergePlot.py

--- C/Merge sort/MergePlot.py
+++ C/Merge sort/MergePlot.py
@@ -1,8 +1,4 @@
 
-    # @Author: Raghav Maheshwari.
-    # @Date: 26th May, 2019
-    #  @Topic: Merge Sort
-
 import matplotlib.pyplot as plt
 X = [50000,100000,150000,200000,250000]
 Y = [0.036620,0.047661,0.060881,0.069485,0.079553]
