97999d577bab16c56480b1a395fec95c20a6f963
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-10-12 13:34:27 +0530

    Add files via upload

---
+++ C/Warshall's Algorithm/warshall.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,j,k;
+	printf("enter the no of vertices\n");
+	scanf("%d",&n);
+	int A[n][n];
+	printf("enter the adjacency mattix\n");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&A[i][j]);
+		}
+	}
+	for(k=0;k<n;k++)
+	{
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				A[i][j]=(A[i][j] || (A[i][k] && A[k][j]));
+			}
+		}
+	}
+	printf("the transitive closure is \n");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("%d\t",A[i][j]);
+		}
+	printf("\n");
+	}
+}
+
+
+				
