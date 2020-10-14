458e3453de5006cc1edc9f1b4639211a40e06df2
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-10-14 13:07:11 +0530

    Add files via upload

---
+++ C/Floyd's Algorithm/floyd.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int min(int a, int b)
+{
+	if(a<b)
+		return a;
+	else 
+		return b;
+}
+void main()
+{
+	int n,i,j,k;
+	printf("enter the no of vertices\n");
+	scanf("%d",&n);
+	int A[n][n];
+	printf("enter the weight matrix\n");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&A[i][j]);
+		}
+	}
+	for(k=0;k<n;k++)
+	{
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				A[i][j]=min((A[i][j]),(A[i][k]+A[k][j]));
+			}
+		}
+	}
+	printf("shortest path matrix is:\n");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("%d\t",A[i][j]);
+		}
+		printf("\n");
+	}
+}
+
