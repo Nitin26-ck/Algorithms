9e6e0b693c8b1d4ae5e311f58aa5f8a1a15a5dcf
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-10-22 13:46:20 +0530

    Add files via upload

---
+++ Java/Merge Sort/MergePlot.py
@@ -0,0 +1,10 @@
+
+    # @Author: Raghav Maheshwari.
+    # @Date: 26th May, 2019
+    #  @Topic: Merge Sort
+
+import matplotlib.pyplot as plt
+X = [50000,100000,150000,200000,250000]
+Y = [0.036620,0.047661,0.060881,0.069485,0.079553]
+plt.plot(X,Y)
+plt.show()
