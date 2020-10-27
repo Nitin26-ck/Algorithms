b860b096f7b344f82cfbad6a46401d612c3e1a50
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-10-27 13:31:47 +0530

    Add files via upload

---
+++ Java/Quick Sort/QuickPlot.py
@@ -0,0 +1,10 @@
+
+    # @Author: Raghav Maheshwari.
+    # @Date: 26th May, 2019
+    # @Topic: Quick Sort
+
+import matplotlib.pyplot as plt
+X = [100000,200000,300000,400000,500000]
+Y = [0.050045,0.166751,0.359966,0.614697,0.980409]
+plt.plot(X,Y)
+plt.show()
