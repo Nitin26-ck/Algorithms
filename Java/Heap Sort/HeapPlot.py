0cfadcf026c3b643f851149cc810fd0e752e7652
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-11-03 13:29:07 +0530

    Add files via upload

---
+++ Java/Heap Sort/HeapPlot.py
@@ -0,0 +1,10 @@
+
+    # @Author: Raghav Maheshwari.
+    # @Date: 26th May, 2019
+    # @Topic: Heap Sort
+
+import matplotlib.pyplot as plt
+X = [100000,200000,300000,400000,500000]
+Y = [0.035498,0.071268,0.125337,0.153108,0.203232]
+plt.plot(X,Y)
+plt.show()
